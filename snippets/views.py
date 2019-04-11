a027c16ac94f3a931cc8f4dbd363ab3822554d30
Kasey Tiller <kntiller@ccicustom.com>
2019-04-10 21:34:39 -0400

    Added comments to views.py

--- snippets/views.py
+++ snippets/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def index(request):
     
     return render(request, 'index.html')
+
+def user_profile(request):
+
+    return render(request, 'user_profile.html')
+
+def discover_page(request):
+
+    return render(request, 'discover_page.html')
