e1d11d5cfcb9dd92b77d7949c84b47fe227b2215
Kasey Tiller <kntiller@ccicustom.com>
2019-04-10 22:48:37 -0400

    Created Registration Form

--- snippets/admin.py
+++ snippets/admin.py
@@ -6,8 +6,24 @@ from .models import User, Snippet, Folder, Language, Comment
 
 admin.site.register(User, UserAdmin)
 
+@admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'snippet_code', 'source_url', 'link', 'language')
+    list_display = ('title', 'user', 'snippet_code', 'source_url', 'language', 'public', 'favorite',)
     exclude = ('slug',)
+    
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment_body', 'snippet', 'user',)
+
+
 
 
